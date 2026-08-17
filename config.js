6d92a3247b1ed187251929634f44a41a249a3907
homemadewithlovebynd-hub <homemadewithlovebynd@gmail.com>
2026-08-17 16:46:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.SHOP_CONFIG = {
-  SUPABASE_URL: "https://zvdqlnsgwtqcdinwfawf.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://zvdqlnsgwtqcdinwfawf.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_dfPsrJZM7-Im6QPquZx7cA_89Yn4_Du",
   WHATSAPP_NUMBER: "918828078702"
 };
